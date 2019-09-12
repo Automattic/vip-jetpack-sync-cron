8bae17e365f63d141eac0032100d5c7079c44355
Rebecca Hum <rebecca.hum@automattic.com>
2019-09-12 10:42:13 -0600

    Initial Commit with files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# vip-jetpack-sync-cron
+# VIP Jetpack Sync Cron
+This plugin ensures that Jetpack only syncs on the dedicated cron task, as Jetpack Sync piggybacks on various cron/API requests causing slowdowns on the shutdown hook.
---
+++ vip-jetpack-sync-cron.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+Plugin Name: VIP Jetpack Sync Cron
+Description: This plugin ensures that Jetpack only syncs on the dedicated cron task.
+Version: 2.0
+Author: Rebecca Hum, Automattic
+*/
+
+use Automattic\Jetpack\Sync\Actions;
+use Automattic\Jetpack\Sync\Settings;
+
+class VIP_Jetpack_Sync_Cron {
+
+	const SYNC_INTERVAL_NAME = 'vip_jp_sync_cron_interval';
+
+	/**
+	 * Initiate an instance of this class if one doesn't exist already.
+	 *
+	 * @return VIP_Jetpack_Sync_Cron instance
+	 */
+	static public function init() {
+		if ( ! class_exists( 'Jetpack' ) ) { // Bail if no Jetpack.
+			return;
+		}
+
+		if ( ! Actions::sync_via_cron_allowed() ) { // Bail if no syncing via cron allowed.
+			return;
+		}
+
+		add_filter( 'cron_schedules', [ $this, 'jp_sync_cron_schedule_interval' ] );
+		add_filter( 'jetpack_sync_incremental_sync_interval', [ $this, 'filter_jetpack_sync_interval' ], 999 );
+		add_filter( 'jetpack_sync_full_sync_interval', [ $this, 'filter_jetpack_sync_interval' ], 999 );
+		add_filter( 'jetpack_sync_sender_should_load', [ Settings::class, 'is_doing_cron' ], 999 ); // Short circuit loading of Jetpack sender to sync only on cron.
+	}
+
+	/**
+	 * Filter to add custom interval to schedule.
+	 *
+	 * @param array  $schedules
+	 */
+	public function jp_sync_cron_schedule_interval( $schedules ) {
+		$interval = apply_filters( 
+			'vip_jetpack_sync_cron_interval', 
+			[
+		    	'interval' => 60,
+		    	'display'  => esc_html__( 'Every minute' ),
+			] 
+		);
+
+		$schedules[ self::SYNC_INTERVAL_NAME ] = $interval;
+
+		return $schedules;
+	}
+
+	/**
+	 * Filter to return custom cron interval name.
+	 *
+	 * @param string  $incremental_sync_cron_schedule
+	 */
+	public function filter_jetpack_sync_interval() {
+		return self::SYNC_INTERVAL_NAME;
+	}
+}
+
+add_action( 'after_setup_theme', [ 'VIP_Jetpack_Sync_Cron', 'init' ] ); // Since Jetpack autoloads their classes, we need to hook later.
